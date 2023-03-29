7dc808807cb085a840246a277a2f32bb96ffb96d
huanghaiyan <huanghaiyan@hltn.com>
2023-03-29 14:33:45 +0800

    feat: add chat

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import countryRoutes from "./route/country";
-
+import axios from 'axios'
+import dotenv from 'dotenv'
 const app = express();
+dotenv.config()
+const apiKey = process.env.OPENAI_API_KEY
 const port = process.env.PORT || 3333;
 
 app.use(express.json());
@@ -11,6 +14,23 @@ app.use(express.text({ type: "text/html" }));
 app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
 });
+
+app.post('/message', async(req, res) => {
+  console.log('post method')
+  const  { message } = req.body
+  console.log(message, 111111)
+  const response = await axios.post(`https://api.openai.com/v1/chat/completions`, {
+    "model": "gpt-3.5-turbo-0301",
+    "messages": [{"role": "user", "content": message}]
+  }, {
+    headers: {
+      "Authorization": "Bearer " + apiKey,
+      "Content-Type": "application/json"
+    }
+  })
+  res.json(response)
+})
+
 
 app.use("/countries", countryRoutes);
 
