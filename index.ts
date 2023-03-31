83ac50ce27216c7280a584f1949831c738d50c0e
huanghaiyan <2031499619@qq.com>
2023-03-31 08:30:41 +0800

    feat: handle cors

--- index.ts
+++ index.ts
@@ -9,6 +9,12 @@ const port = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', '*')
+  next()
+})
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
